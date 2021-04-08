49c85a7174f0df237dba23f12ea97f236417158a
Łukasz Frątczak <lukasz_fratczak@wp.pl>
2021-04-08 18:15:26 +0200

    "Fetch more data from internet, update database"

--- app/src/main/java/com/lukasz/cointracker/database/DatabaseEntities.kt
+++ app/src/main/java/com/lukasz/cointracker/database/DatabaseEntities.kt
@@ -13,9 +13,18 @@ data class DatabaseCoin constructor(
     val image: String?,
     val current_price: Double?,
     val market_cap: Double?,
+    val market_cap_rank: Double?,
     val price_change_percentage_1h_in_currency: Double?,
     val price_change_percentage_24h_in_currency: Double?,
     val price_change_percentage_7d_in_currency: Double?,
+    val fully_diluted_valuation: Double?,
+    val high_24h: Double?,
+    val low_24h: Double?,
+    val circulating_supply: Double?,
+    val max_supply: Double?,
+    val ath: Double?,
+    val ath_change_percentage: Double?,
+    val ath_date: String?,
     val total_volume: Double?)
 
 fun List<DatabaseCoin>.asDomainModel(): List<Coin> {
@@ -27,9 +36,18 @@ fun List<DatabaseCoin>.asDomainModel(): List<Coin> {
             image = it.image,
             current_price = it.current_price,
             market_cap = it.market_cap,
+            market_cap_rank = it.market_cap_rank,
             price_change_percentage_1h_in_currency = it.price_change_percentage_1h_in_currency,
             price_change_percentage_24h_in_currency = it.price_change_percentage_24h_in_currency,
             price_change_percentage_7d_in_currency = it.price_change_percentage_7d_in_currency,
+            fully_diluted_valuation = it.fully_diluted_valuation,
+            high_24h = it.high_24h,
+            low_24h = it.low_24h,
+            circulating_supply = it.circulating_supply,
+            max_supply = it.max_supply,
+            ath = it.ath,
+            ath_change_percentage = it.ath_change_percentage,
+            ath_date = it.ath_date,
             total_volume = it.total_volume)
     }
 }
--- app/src/main/java/com/lukasz/cointracker/database/Room.kt
+++ app/src/main/java/com/lukasz/cointracker/database/Room.kt
@@ -3,21 +3,68 @@ package com.lukasz.cointracker.database
 import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.*
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.lukasz.cointracker.database.CoinsDatabase.Companion.MIGRATION_1_2
 
 
 @Dao
 interface CoinDao {
-    @Query("select * from databasecoin")
+    @Query("select * from databasecoin order by market_cap desc")
     fun getCoins(): LiveData<List<DatabaseCoin>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAll( videos: List<DatabaseCoin>)
+    fun insertAll(videos: List<DatabaseCoin>)
 
 }
 
-@Database(entities = [DatabaseCoin::class], version = 1)
-abstract class CoinsDatabase: RoomDatabase() {
+@Database(entities = [DatabaseCoin::class], version = 2)
+abstract class CoinsDatabase : RoomDatabase() {
     abstract val coinDao: CoinDao
+
+    companion object {
+
+        val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL(
+                    "ALTER TABLE databasecoin "
+                            + " ADD COLUMN market_cap_rank REAL"
+                )
+                database.execSQL(
+                    "ALTER TABLE databasecoin "
+                            + " ADD COLUMN fully_diluted_valuation REAL"
+                )
+                database.execSQL(
+                    "ALTER TABLE databasecoin "
+                            + " ADD COLUMN high_24h REAL"
+                )
+                database.execSQL(
+                    "ALTER TABLE databasecoin "
+                            + " ADD COLUMN low_24h REAL"
+                )
+                database.execSQL(
+                    "ALTER TABLE databasecoin "
+                            + " ADD COLUMN circulating_supply REAL"
+                )
+                database.execSQL(
+                    "ALTER TABLE databasecoin "
+                            + " ADD COLUMN max_supply REAL"
+                )
+                database.execSQL(
+                    "ALTER TABLE databasecoin "
+                            + " ADD COLUMN ath REAL"
+                )
+                database.execSQL(
+                    "ALTER TABLE databasecoin "
+                            + " ADD COLUMN ath_change_percentage REAL"
+                )
+                database.execSQL(
+                    "ALTER TABLE databasecoin "
+                            + " ADD COLUMN ath_date TEXT"
+                )
+            }
+        }
+    }
 }
 
 private lateinit var INSTANCE: CoinsDatabase
@@ -29,7 +76,8 @@ fun getDatabase(context: Context): CoinsDatabase {
                 context.applicationContext,
                 CoinsDatabase::class.java,
                 "coins"
-            ).build()
+            ).addMigrations(MIGRATION_1_2)
+                .build()
         }
     }
     return INSTANCE
