4c232e0c7b928a670174a95cdf8d99baf3b0a6b9
Łukasz Frątczak <lukasz_fratczak@wp.pl>
2021-06-09 14:51:42 +0200

    Add "find out more" button to redirect to the page of coin, correct displaying long headers

--- app/src/main/java/com/lukasz/cointracker/detail/DetailFragment.kt
+++ app/src/main/java/com/lukasz/cointracker/detail/DetailFragment.kt
@@ -1,5 +1,7 @@
 package com.lukasz.cointracker.detail
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,17 +12,32 @@ import com.lukasz.cointracker.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
 
+    private val viewModel: DetailViewModel by lazy {
+        val application = requireNotNull(activity).application
+        val coin = DetailFragmentArgs.fromBundle(requireArguments()).selectedCoin
+        val viewModelFactory = DetailViewModelFactory(coin, application)
+        ViewModelProvider(
+            this, viewModelFactory).get(DetailViewModel::class.java)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         val binding = FragmentDetailBinding.inflate(inflater)
-        val application = requireNotNull(activity).application
 
         binding.lifecycleOwner = this
 
-        val coin = DetailFragmentArgs.fromBundle(requireArguments()).selectedCoin
-        val viewModelFactory = DetailViewModelFactory(coin, application)
-        binding.viewModel = ViewModelProvider(
-            this, viewModelFactory).get(DetailViewModel::class.java)
+        binding.viewModel = viewModel
+
+        viewModel.navigateToCoinGeckoUrl.observe(viewLifecycleOwner, {
+            if (null != it) {
+                val webIntent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(it)
+                )
+                requireActivity().startActivity(webIntent)
+                viewModel.displayInWebBrowserComplete()
+            }
+        })
 
         return binding.root
     }
--- app/src/main/java/com/lukasz/cointracker/detail/DetailViewModel.kt
+++ app/src/main/java/com/lukasz/cointracker/detail/DetailViewModel.kt
@@ -6,11 +6,27 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.lukasz.cointracker.domain.Coin
 import com.lukasz.cointracker.network.NetworkCoin
+import java.util.*
 
 class DetailViewModel(coin: Coin, app: Application) : AndroidViewModel(app) {
     private val _selectedCoin = MutableLiveData<Coin>()
     val selectedCoin: LiveData<Coin>
         get() = _selectedCoin
+
+    private val _navigateToCoinGeckoUrl = MutableLiveData<String>()
+    val navigateToCoinGeckoUrl: LiveData<String>
+        get() = _navigateToCoinGeckoUrl
+
+    fun displayInWebBrowser(name: String) {
+        val re = Regex("[^A-Za-z0-9]")
+        var validName = re.replace(name, "-")
+        validName = validName.toLowerCase(Locale.getDefault())
+        _navigateToCoinGeckoUrl.value = "https://www.coingecko.com/pl/waluty/$validName"
+    }
+
+    fun displayInWebBrowserComplete() {
+        _navigateToCoinGeckoUrl.value = null
+    }
 
     init {
         _selectedCoin.value = coin
